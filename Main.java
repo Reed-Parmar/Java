231cb0cca6591dc3aee72f47caf165dd20647449
Reedham0806 <reedhamparmar@gmail.com>
2025-06-20 17:04:30 +0530

    new stuff

--- Main.java
+++ Main.java
@@ -1,5 +1,27 @@
+
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        //output
+        System.out.print("Hello World");
+        System.out.println("\nHello World");
+        //variables
+        String name= "TONAY STARK";
+        int a=5;
+        int b=10;
+        System.out.println(name);
+        int sum=a-b;
+        System.out.println(sum);
+        //input
+        //nextInt
+        //nextFloat
+        System.out.println("hello");
+        System.out.println("Enter 2 numbers: ");
+        Scanner sc= new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int hii=x+y;
+        System.out.println(hii);
     }
 }
