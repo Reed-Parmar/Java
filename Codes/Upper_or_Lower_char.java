4efcc3b86f5c1888d19f8c6d897fe84d83292c5f
Reed-Parmar <reedhamparmar@gmail.com>
2025-07-01 16:21:41 +0530

    new stuff

--- Codes/Upper_or_Lower_char.java
+++ Codes/Upper_or_Lower_char.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Upper_or_Lower_char {
     public static void main(String[] args) {
         Scanner in= new Scanner (System.in);
+        @SuppressWarnings("unused")
         char ch=in.next().trim().charAt(0);
         
     }
