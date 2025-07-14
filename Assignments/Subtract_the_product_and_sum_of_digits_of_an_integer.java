5f98b7378387eb8d0b2316da465584ba7109d0ba
Reed-Parmar <reedhamparmar@gmail.com>
2025-07-15 00:24:29 +0530

    new stuff

---
+++ Assignments/Subtract_the_product_and_sum_of_digits_of_an_integer.java
@@ -0,0 +1,12 @@
+package Assignments;
+
+import java.util.Scanner;
+
+public class Subtract_the_product_and_sum_of_digits_of_an_integer {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n =in.nextInt();
+                
+    }
+}
---
+++ out/production/Learn Programming/README.md
@@ -0,0 +1,4 @@
+# Java
+Learning Java
+<br>
+New Repo
